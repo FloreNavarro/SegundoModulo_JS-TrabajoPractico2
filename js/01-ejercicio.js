767dff0d83b636c4c8361161f275f5d7c7c0bebc
Florencia Navarro <fmnavarro1o11@gmail.com>
2026-03-11 08:55:51 -0300

    01-ejercicio-pida al usuario la edad de conducir

--- js/01-ejercicio.js
+++ js/01-ejercicio.js
@@ -1,8 +1,16 @@
 //Escribir un programa que solicite la edad y si es mayor de 18 años mostrar un 
 //mensaje que ya puede conducir, si la edad ingresada no es un número válido 
 // indicarlo en un mensaje.
-const edad = prompt("Ingrese su edad")
+const edad = parseInt(prompt("Ingrese su edad"))
 
-if (edad > 18){
+if (isNaN(edad)){
+ alert ("El valor ingresado no es correcto")
+}
+
+if (edad >= 18){
     alert("Puede conducir")
+}else{
+
+alert("Es menor de edad, no puede conducir")
+
 }
