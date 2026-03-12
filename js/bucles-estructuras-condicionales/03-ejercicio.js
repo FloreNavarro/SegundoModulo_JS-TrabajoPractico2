a0c9aad01d543b5ac81c299d49808c41bdaf0244
Florencia Navarro <fmnavarro1o11@gmail.com>
2026-03-12 12:04:40 -0300

    ejercicio donde se pide la frase y se concatena

--- js/bucles-estructuras-condicionales/03-ejercicio.js
+++ js/bucles-estructuras-condicionales/03-ejercicio.js
@@ -11,15 +11,8 @@
 let nuevacadena = "";
 while (confirm("¿Desea ingresar una frase? ")) {
   let cadena = prompt("Ingrese una frase:");
-  //nuevacadena += cadena;
-  if (cadena !== null & cadena !== "") {
-    
-    if (nuevacadena === "") {
-      nuevacadena = cadena;
-    } else {
-      nuevacadena += "-" + cadena;
-    }
-  }
+  nuevacadena += cadena;
+  
 }
 
 alert("El texto ingresado fue: " + nuevacadena);
