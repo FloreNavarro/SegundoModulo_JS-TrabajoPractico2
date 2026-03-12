8e9675b570df0eebe8a0212268944ece4faa55cc
Florencia Navarro <fmnavarro1o11@gmail.com>
2026-03-12 11:57:26 -0300

    guardar ejercicio 03 finalizado

--- js/bucles-estructuras-condicionales/03-ejercicio.js
+++ js/bucles-estructuras-condicionales/03-ejercicio.js
@@ -1,7 +1,6 @@
 //3- Realiza un script que pida cadenas de texto  hasta que se pulse “cancelar”.
 //  Al salir con “cancelar” deben mostrarse todas las cadenas concatenadas con un guión -.
 
-
 /*do{
 
   let cadena= prompt("Ingrese un frase o cancelar para salir: " )
@@ -9,12 +8,18 @@
 }while(confirm("Ingrese un frase o cancelar para salir")  )
 
     alert("El texto ingresado fue: " + nuevacadena)*/
-     let nuevacadena = " " ; 
-    while( confirm("Ingrese un frase:") ){
-
-      let cadena= prompt("Ingrese un frase o cancelar para salir: " )
-             nuevacadena += cadena;
-
+let nuevacadena = "";
+while (confirm("¿Desea ingresar una frase? ")) {
+  let cadena = prompt("Ingrese una frase:");
+  //nuevacadena += cadena;
+  if (cadena !== null & cadena !== "") {
+    
+    if (nuevacadena === "") {
+      nuevacadena = cadena;
+    } else {
+      nuevacadena += "-" + cadena;
     }
+  }
+}
 
-    alert("El texto ingresado fue: " + nuevacadena)
+alert("El texto ingresado fue: " + nuevacadena);
