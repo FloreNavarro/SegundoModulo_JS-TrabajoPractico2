184da42c157afc28cf28829f86e772fafa9b1180
Florencia Navarro <fmnavarro1o11@gmail.com>
2026-03-16 19:45:44 -0300

    feat: completar ejercicio 11 y vincular en index

--- js/bucles-estructuras-condicionales/11-ejercicio.js
+++ js/bucles-estructuras-condicionales/11-ejercicio.js
@@ -1,2 +1,5 @@
 //Realiza un script que pida por teclado 3 edades y 3 nombres e indique el nombre del mayor. funcion Math() 
-
+const num = prompt("Ingrese un numero: ")
+ for(let i=1; i <= num; i++){
+      document.writeln(i + "<br>")
+ }
